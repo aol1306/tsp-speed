e0cd94d0dcb67fdb22426957a7e1ed1b7e21e759
aol1306 <jdzieciatko@gmail.com>
2019-06-02 14:22:09 +0200

    Remove unused function

--- go/main.go
+++ go/main.go
@@ -69,34 +69,6 @@ func readCoordinates() coordSlice {
 	return ret
 }
 
-func permutations(arr coordSlice) []coordSlice {
-	var helper func(coordSlice, int)
-	res := []coordSlice{}
-
-	helper = func(arr coordSlice, n int) {
-		if n == 1 {
-			tmp := make(coordSlice, len(arr))
-			copy(tmp, arr)
-			res = append(res, tmp)
-		} else {
-			for i := 0; i < n; i++ {
-				helper(arr, n-1)
-				if n%2 == 1 {
-					tmp := arr[i]
-					arr[i] = arr[n-1]
-					arr[n-1] = tmp
-				} else {
-					tmp := arr[0]
-					arr[0] = arr[n-1]
-					arr[n-1] = tmp
-				}
-			}
-		}
-	}
-	helper(arr, len(arr))
-	return res
-}
-
 func solveBruteForce() {
 	coords := readCoordinates()
 	startTime := time.Now()
